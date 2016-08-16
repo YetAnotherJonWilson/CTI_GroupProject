8bb4031a3cbd90042f1cdce22ff445182a172945
Trent Johnson <tdjohnson0722@gmail.com>
2016-08-16 16:41:15 -0500

    index.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var passport = require('passport');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var LocalStrategy = require('passport-local');
+var index = require('./routes/index');
 var app = express();
 
-
-
-
+//parse request
+app.use(bodyParser.json());
+//serve static files
+app.use(express.static('public'));
+//routes
+app.use('/', index);
 
 
 var server = app.listen(3000, function(){
