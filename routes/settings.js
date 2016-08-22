f2c595f964198e0ef8c85aeee4cf3aecd305da12
Brian Verduzco <brian.j.verduzco@gmail.com>
2016-08-22 16:57:53 -0500

    .brian is more queries for sf

---
+++ routes/settings.js
@@ -0,0 +1,15 @@
+var router = require('express').Router();
+var path = require('path');
+var multer = require('multer');
+// var uploading = multer({
+//   dest: __dirname + '../public/assets/',
+//   //limits filesize == 1MB
+//   limits: {fileSize: 1000000, files: 1}
+// });
+
+
+// router.post('/upload', uploading, function(request, response){
+
+// });
+
+module.exports = router;
