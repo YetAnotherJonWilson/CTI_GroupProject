ecdfdfab8dce0e150f5fb6d20fe16d6aa701ecf3
Jon Wilson <jonwilsoncomedy@gmail.com>
2016-08-24 14:18:31 -0500

    removed ngUpload

--- client/SettingsController.js
+++ client/SettingsController.js
@@ -1,6 +1,10 @@
 angular.module('App').controller('SettingsController', ['$http', '$location', 'DataService' , function($http, $location, DataService){
 
+var vm = this;
 
+vm.complete = function(content) {
+    console.log(content); // process content
+}
 
 
 }]);
