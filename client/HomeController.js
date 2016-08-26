0a5d10efba3c1fa3801e9dd950b7c2206b453094
Ryan Mattson <ryansmattson@gmail.com>
2016-08-26 11:05:52 -0500

    progress on edit view

--- client/HomeController.js
+++ client/HomeController.js
@@ -1,11 +1,10 @@
-angular.module('App').controller('HomeController', ['$http', '$location', 'DataService', 'RouteService', 'orderByFilter', '$uibModal', 'TemplateService', 'EmailService', function($http, $location, DataService, RouteService, orderBy, $uibModal,TemplateService,EmailService) {
+angular.module('App').controller('HomeController', ['$http', '$location', 'DataService', 'RouteService', 'orderByFilter', '$uibModal', 'TemplateService', 'EmailService', function($http, $location, DataService, RouteService, orderBy, $uibModal, TemplateService, EmailService) {
 
 	DataService.getData();
 
 	var vm = this;
 
 	vm.donorList = DataService.sortedObject.sorted;
-
 
 	vm.homeRoute = function() {
 		RouteService.homeRoute();
@@ -32,15 +31,49 @@ angular.module('App').controller('HomeController', ['$http', '$location', 'DataS
 		vm.propertyName = propertyName;
 	};
 
-//////////EDIT VIEW/////////
+	//////////EDIT VIEW/////////
 
 
-vm.templatesObject = TemplateService.templatesObject;
-vm.currentTemplate = TemplateService.currentTemplate;
-vm.savedEmails = TemplateService.savedEmails;
-vm.imagesArray = TemplateService.imagesObject.images;
+	vm.templatesObject = TemplateService.templatesObject;
+	vm.currentTemplate = TemplateService.currentTemplate;
+	vm.savedEmails = TemplateService.savedEmails;
+	vm.imagesArray = TemplateService.imagesObject.images;
 
-vm.fieldId = '';
+	vm.fieldId = '';
+
+
+
+	vm.currentDonor = {};
+
+	vm.setCurrentEditView = function(id) {
+		var tempDonor;
+		var tempIndex;
+
+		for (var i = 0; i < vm.donorList.length; i++) {
+			if (donorList[i].id = id) {
+				tempDonor = donorList[i];
+				tempIndex = i;
+			}
+		}
+
+		vm.currentDonor = tempDonor;
+	}
+
+	//if it's been edited, pull from the temp array of edited emails
+	//get selected template
+	//get edited paragraphs
+	//get saved donor info
+	//else pull fresh from DataService
+	//get user standard template.html
+	//get standard template paragraphs
+	//get donor information
+
+
+
+
+
+
+
 
 
 vm.editObject = {};
@@ -63,13 +96,13 @@ vm.editObject = {};
 
 function createEditObject() {
 
-  vm.editObject.name = vm.donor.name;
-  vm.editObject.email = vm.donor.email;
-  vm.editObject.amount = vm.donor.amount;
-  vm.editObject.donate_date = vm.donor.donate_date;
-  vm.editObject.informal_greeting = vm.donor.informal_greeting;
-  vm.editObject.template = vm.user.template;
-  vm.editObject.templateContents = vm.template[vm.editObject.template];
+	vm.editObject.name = vm.donor.name;
+	vm.editObject.email = vm.donor.email;
+	vm.editObject.amount = vm.donor.amount;
+	vm.editObject.donate_date = vm.donor.donate_date;
+	vm.editObject.informal_greeting = vm.donor.informal_greeting;
+	vm.editObject.template = vm.user.template;
+	vm.editObject.templateContents = vm.template[vm.editObject.template];
 }
 
 function chooseTemplate() {}
@@ -77,14 +110,14 @@ function chooseTemplate() {}
 
 
 vm.templates = [{
-  name: 'Template 1',
-  url: 'emails/template1EditView.html'
+	name: 'Template 1',
+	url: 'emails/template1EditView.html'
 }, {
-  name: 'Template 2',
-  url: 'emails/template2EditView.html'
+	name: 'Template 2',
+	url: 'emails/template2EditView.html'
 }, {
-  name: 'Template 3',
-  url: 'emails/template3EditView.html'
+	name: 'Template 3',
+	url: 'emails/template3EditView.html'
 }];
 
 vm.template = vm.templates[0];
@@ -92,65 +125,64 @@ vm.template = vm.templates[0];
 
 
 vm.editModal = function(id) {
-  console.log('currentTemplate:', vm.currentTemplate);
+	console.log('currentTemplate:', vm.currentTemplate);
 
-  vm.fieldId = id;
-  vm.currentTemplate.currentField = id;
-  $uibModal.open({
-    animation: true,
-    ariaLabelledBy: 'edit text modal',
-    ariaDescribedBy: 'edit text',
-    templateUrl: 'emails/edit_modal.html',
-    controller: 'ModalController',
-    controllerAs: 'modal',
-    size: 'md',
-    // resolve: {
-    // 	items: function() {
-    // 		return vm.items;
-    // 	}
-    // }
-  });
+	vm.fieldId = id;
+	vm.currentTemplate.currentField = id;
+	$uibModal.open({
+		animation: true,
+		ariaLabelledBy: 'edit text modal',
+		ariaDescribedBy: 'edit text',
+		templateUrl: 'emails/edit_modal.html',
+		controller: 'ModalController',
+		controllerAs: 'modal',
+		size: 'md',
+		// resolve: {
+		// 	items: function() {
+		// 		return vm.items;
+		// 	}
+		// }
+	});
 
-  //
-  // 	modalInstance.result.then(function(selectedItem) {
-  // 		vm.selected = selectedItem;
-  // 	}, function() {
-  // 		$log.info('Modal dismissed at: ' + new Date());
-  // 	});
+	//
+	// 	modalInstance.result.then(function(selectedItem) {
+	// 		vm.selected = selectedItem;
+	// 	}, function() {
+	// 		$log.info('Modal dismissed at: ' + new Date());
+	// 	});
 };
 
 
 vm.imageModal = function(id) {
 
-  // vm.fieldId = id;
-  // vm.currentTemplate.currentField = id;
-  $uibModal.open({
-    animation: true,
-    ariaLabelledBy: 'image modal',
-    ariaDescribedBy: 'pick an image',
-    templateUrl: 'emails/image_modal.html',
-    controller: 'ModalController',
-    controllerAs: 'modal',
-    size: 'md'
-  });
+	// vm.fieldId = id;
+	// vm.currentTemplate.currentField = id;
+	$uibModal.open({
+		animation: true,
+		ariaLabelledBy: 'image modal',
+		ariaDescribedBy: 'pick an image',
+		templateUrl: 'emails/image_modal.html',
+		controller: 'ModalController',
+		controllerAs: 'modal',
+		size: 'md'
+	});
 };
 
 
 vm.sendMail = function(p1, q, p2) {
-  console.log('You cliked me');
-  EmailService.sendMail(p1, q, p2);
+	console.log('You cliked me');
+	EmailService.sendMail(p1, q, p2);
 }
 
-vm.saveEditedEmail = function(p1, p2, p3, p4, q, ps){
-  TemplateService.saveEditedEmail(p1, p2, p3, p4, q, ps)
+vm.saveEditedEmail = function(p1, p2, p3, p4, q, ps) {
+	TemplateService.saveEditedEmail(p1, p2, p3, p4, q, ps)
 }
 
 function getCurrentTemplate(templateNum) {
-  console.log('Im getting the current template');
-  TemplateService.getCurrentTemplate(templateNum);
+	console.log('Im getting the current template');
+	TemplateService.getCurrentTemplate(templateNum);
 }
 
-getCurrentTemplate(1);
 
 
 
